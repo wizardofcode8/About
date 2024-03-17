30d5814d5973bbf0332bc7853758acd7371cf6f9
Ikram khan <ikcode4599@gmail.com>
2024-03-17 06:06:22 +0500

    newone

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -4,7 +4,7 @@ const Screen = () => {
   return (
     <>
     <div class="container mx-auto flex justify-center">
-  <div class="images w-1/2">
+  <div class="images w-full md:w-1/2">
     <img src="/images/Mac Studio and Studio Display.png" alt="screen" />   
   </div>
 </div>
